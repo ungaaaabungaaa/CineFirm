cdffaa0a846df6a2705e2e3dd83d8a58f07d1bb6
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-06-15 14:53:21 +0530

    added contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -15,7 +15,10 @@ const ContactUs = () => {
         <div className='context'>
           <h1 className='contact_us_h1'>Contact US</h1>
           <br></br>
-          <p className='contact_us_p'>Get in touch with us for any photography or videography <br></br> requirement. We specialise in food shoots, corporate <br></br> events, commercials, weddings and social media videos. <br></br> Contact us at +91-9885859637 or send an email to  <br></br>
+          <p className='contact_us_p'>Get in touch with us for any photography or videography <br></br>
+           requirement. We specialise in food shoots, corporate <br></br>
+            events, commercials, weddings and social media videos. <br></br>
+             Contact us at +91-9885859637 or send an email to  <br></br>
           cinefirm@gmail.com</p>
         </div>
        </div>
