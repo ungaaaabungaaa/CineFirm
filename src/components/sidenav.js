83302c88254b711c086efb465e5404e40f355118
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-06-15 14:44:06 +0530

    about us page

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -4,15 +4,15 @@ import '../styles/sidenav.css';
 const SideNav = () => {
   return (
     <div className='SideNav'>
-      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/cinefirm_official/" target="_blank" rel="noopener noreferrer">
         <span className="mdi--instagram"></span>
       </a>
-      <br />
-      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+      <br/>
+      <a href="https://x.com/cinefirm" target="_blank" rel="noopener noreferrer">
         <span className="pajamas--twitter"></span>
       </a>
-      <br />
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+      <br/>
+      <a href="https://www.facebook.com/cinefirm/" target="_blank" rel="noopener noreferrer">
         <span className="gg--facebook"></span>
       </a>
     </div>
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,12 +4,29 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import SideNav from '../components/sidenav';
 import transition from '../transition';
+import '../styles/aboutus.css'
 
 const AboutUs = () => {
   return (
     <div>
       <Header></Header>
       <SideNav></SideNav>
+      <div className='aboutus_section'>
+        <div className='aboutus-text'>
+           <h1>
+            Cinefirm  <br></br>
+            A Photography & <br></br>
+            Videography company.
+           </h1>
+           <p>We are passionate about our craft & dedication <br></br>
+            towards art. We chose this path with the <br></br>
+             confidence on our work & adaptability to the <br></br> tough conditions that we face while we work</p>
+        </div>
+
+        <div className='aboutus-grid'>
+           
+        </div>
+      </div>
       <Footer></Footer>
     </div>
   );
