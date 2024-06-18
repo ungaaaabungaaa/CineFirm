a357866b1a5a9475813a721eef85a3f5a5331ba5
Syed Abdul Muqeeth <samx123786@gmail.com>
2024-06-18 15:33:59 +0530

    added few more fixes to the bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,8 @@
-// src/components/Home.js
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import SideNav from '../components/sidenav';
-import '../styles/home.css'
+import '../styles/home.css';
 import transition from '../transition';
 import slide1 from '../res/slider/1.webp';
 import slide2 from '../res/slider/2.webp';
@@ -11,149 +10,118 @@ import slide3 from '../res/slider/3.webp';
 import slide4 from '../res/slider/4.webp';
 import slide5 from '../res/slider/5.webp';
 
+const slides = [slide1, slide2, slide3, slide4, slide5];
+
+const slideContents = [
+  {
+    author: 'Author 1',
+    name: 'Title 1',
+    desc: 'Description 1',
+  },
+  {
+    author: 'Author 2',
+    name: 'Title 2',
+    desc: 'Description 2',
+  },
+  {
+    author: 'Author 3',
+    name: 'Title 3',
+    desc: 'Description 3',
+  },
+  {
+    author: 'Author 4',
+    name: 'Title 4',
+    desc: 'Description 4',
+  },
+  {
+    author: 'Author 5',
+    name: 'Title 5',
+    desc: 'Description 5',
+  },
+];
+
 const Home = () => {
+  const carouselRef = useRef(null);
+  const sliderRef = useRef(null);
+  const thumbnailRef = useRef(null);
+  const timeRunning = 3000;
+  const timeAutoNext = 7000;
+  let runNextAuto;
+
+  useEffect(() => {
+    const nextDom = document.getElementById('next');
+    const prevDom = document.getElementById('prev');
+
+    const showSlider = (type) => {
+      const sliderItemsDom = sliderRef.current.children;
+      const thumbnailItemsDom = thumbnailRef.current.children;
+
+      if (type === 'next') {
+        sliderRef.current.appendChild(sliderItemsDom[0]);
+        thumbnailRef.current.appendChild(thumbnailItemsDom[0]);
+        carouselRef.current.classList.add('next');
+      } else {
+        sliderRef.current.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+        thumbnailRef.current.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+        carouselRef.current.classList.add('prev');
+      }
+
+      setTimeout(() => {
+        carouselRef.current.classList.remove('next');
+        carouselRef.current.classList.remove('prev');
+      }, timeRunning);
+
+      clearTimeout(runNextAuto);
+      runNextAuto = setTimeout(() => {
+        nextDom.click();
+      }, timeAutoNext);
+    };
+
+    nextDom.onclick = () => showSlider('next');
+    prevDom.onclick = () => showSlider('prev');
+
+    runNextAuto = setTimeout(() => {
+      nextDom.click();
+    }, timeAutoNext);
+
+    return () => {
+      nextDom.onclick = null;
+      prevDom.onclick = null;
+      clearTimeout(runNextAuto);
+    };
+  }, [timeRunning, timeAutoNext]);
+
   return (
     <div>
-      <Header></Header>
-      <SideNav></SideNav>
-      <Footer></Footer>
-      <div className='carousel'>
-         <div className='list'>
-              <div className='item'>
-                <img src={slide1} alt="slider 1"/>
-                <div className='content'>
-                <div class="author">CineFrim</div>
-                    <div class="title">Big Bro's</div>
-                    <div class="topic">Food PhotoGraphy</div>
-                    <div class="des">
-                        This image presents a delicious pizza on a rustic wooden board, topped with melted cheese, pepperoni, basil, and vegetables, against a textured wooden table. Surrounding the pizza are fresh ingredients: tomatoes, garlic, basil, olive oil, grated cheese, bell peppers, and bowls of crushed red pepper, oregano, and parmesan, all highlighted by warm, inviting light.
-                    </div>
-                    <div class="buttons">
-                        <button>SEE MORE</button>
-                        <button>Contact Us</button>
-                    </div>
-                </div>
+      <Header />
+      <SideNav />
+      <Footer />
+      <div className="carousel" ref={carouselRef}>
+        <div className="list" ref={sliderRef}>
+          {slides.map((slide, index) => (
+            <div className="item" key={index}>
+              <img src={slide} alt={`slider ${index + 1}`} />
+              <div className="content">
+                <div className="author">{slideContents[index].author}</div>
+                <div className="title">{slideContents[index].name}</div>
+                <div className="topic">Food Photography</div>
+                <div className="des">{slideContents[index].desc}</div>
               </div>
-              <div className='item'>
-                <img src={slide2} alt="slider 2"/>
-                <div className='content'>
-                <div class="author">LUNDEV</div>
-                    <div class="title">DESIGN SLIDER</div>
-                    <div class="topic">ANIMAL</div>
-                    <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div class="buttons">
-                        <button>SEE MORE</button>
-                        <button>SUBSCRIBE</button>
-                    </div>
-                </div>
-              </div>
-              <div className='item'>
-                <img src={slide3} alt="slider 3"/>
-                <div className='content'>
-                <div class="author">LUNDEV</div>
-                    <div class="title">DESIGN SLIDER</div>
-                    <div class="topic">ANIMAL</div>
-                    <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div class="buttons">
-                        <button>SEE MORE</button>
-                        <button>SUBSCRIBE</button>
-                    </div>
-                </div>
-              </div>
-              <div className='item'>
-                <img src={slide4} alt="slider 4"/>
-                <div className='content'>
-                <div class="author">LUNDEV</div>
-                    <div class="title">DESIGN SLIDER</div>
-                    <div class="topic">ANIMAL</div>
-                    <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div class="buttons">
-                        <button>SEE MORE</button>
-                        <button>SUBSCRIBE</button>
-                    </div>
-                </div>
-              </div>
-              <div className='item'>
-                <img src={slide5} alt="slider 5"/>
-                <div className='content'>
-                <div class="author">LUNDEV</div>
-                    <div class="title">DESIGN SLIDER</div>
-                    <div class="topic">ANIMAL</div>
-                    <div class="des">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?
-                    </div>
-                    <div class="buttons">
-                        <button>SEE MORE</button>
-                        <button>SUBSCRIBE</button>
-                    </div>
-                </div>
-              </div>
-         </div>
+            </div>
+          ))}
+        </div>
       </div>
 
-      <div className="thumbnail">
-            <div className="item">
-                <img src={slide1} />
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
+      <div className="thumbnail" ref={thumbnailRef}>
+        {slides.map((slide, index) => (
+          <div className="item" key={index}>
+            <img src={slide} alt={`thumbnail ${index + 1}`} />
+            <div className="content">
+              <div className="title">{slideContents[index].name}</div>
+              <div className="description">{slideContents[index].desc}</div>
             </div>
-            <div className="item">
-                <img src={slide2} />
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src={slide3} />
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-            <div className="item">
-                <img src={slide4} />
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
-
-            <div className="item">
-                <img src={slide5} />
-                <div className="content">
-                    <div className="title">
-                        Name Slider
-                    </div>
-                    <div className="description">
-                        Description
-                    </div>
-                </div>
-            </div>
+          </div>
+        ))}
       </div>
 
       <div className="arrows">
@@ -162,7 +130,6 @@ const Home = () => {
       </div>
 
       <div className="time"></div>
-        
     </div>
   );
 };
